01aa54c67ea1a71199f7a4d8081446d5ac8a2280
Jennie <m65pjj@hotmail.com>
2020-03-05 15:56:15 +0800

    Refactor Unit Category Page

--- src/main/TC_UnitCategory.js
+++ src/main/TC_UnitCategory.js
@@ -1,10 +1,7 @@
 import React, { Fragment } from "react";
-import {Route, Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from "../components/common/header";
 import "../css/TC_UnitCategory.css";
-// import { connect } from "react-redux";
-// import { compose } from "redux";
-// import { firestoreConnect } from "react-redux-firebase";
 
 class TC_UnitCategory extends React.Component{
     render(){
@@ -14,12 +11,12 @@ class TC_UnitCategory extends React.Component{
                 <div className="UnitCatalogContainer">
                     <h2>Common <br/>Chinese Vocabulary</h2>
                     <ul>                    
-                        <Link to={{pathname:"/vocabulary", state:"greetings"}}><li className="box1">Greetings</li></Link>
-                        <Link to={{pathname:"/vocabulary", state:"number"}}><li className="box2">Number</li></Link>
-                        <Link to={{pathname:"/vocabulary", state:"shopping"}}><li className="box3">Shopping</li></Link>
-                        <Link to={{pathname:"/vocabulary", state:"travel"}}><li className="box4">Travel</li></Link>
-                        <Link to={{pathname:"/vocabulary", state:"leisure"}}><li className="box5">Leisure</li></Link>
-                        <Link to={{pathname:"/vocabulary", state:"emotion"}}><li className="box6">Emotion</li></Link>
+                        <Link to={{ pathname:"/vocabulary", state:"greetings" }}><li className="box1">Greetings</li></Link>
+                        <Link to={{ pathname:"/vocabulary", state:"number" }}><li className="box2">Number</li></Link>
+                        <Link to={{ pathname:"/vocabulary", state:"shopping" }}><li className="box3">Shopping</li></Link>
+                        <Link to={{ pathname:"/vocabulary", state:"travel" }}><li className="box4">Travel</li></Link>
+                        <Link to={{ pathname:"/vocabulary", state:"leisure" }}><li className="box5">Leisure</li></Link>
+                        <Link to={{ pathname:"/vocabulary", state:"emotion" }}><li className="box6">Emotion</li></Link>
                     </ul>
                 </div>
             </Fragment>
@@ -27,23 +24,5 @@ class TC_UnitCategory extends React.Component{
         )
     }
 }
-// const mapStateToProps = ( state ) => {
-//     console.log(state)
-// return{
-//     number: state.firestore
-// }
-// }
-
-// export default compose( 
-//     firestoreConnect(() => [
-//      {
-//         collection: "Topics",
-//         doc: "lesson",
-//         subcollections: [{collection: "number"}],
-//         storeAs: "number"
-//       }
-//     ]),
-//     connect( mapStateToProps, null )
-// )( TC_UnitCategory )
 
 export default TC_UnitCategory
